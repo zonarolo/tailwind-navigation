79c5dcbcdfda203bfd94ce91c47452396330dd3e
Juan Rolo <juanluis211@gmail.com>
2022-12-23 12:44:26 +0100

    Add navigation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SideBar from './components/SideBar/SideBar'
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs'
+import { FaFire, FaPoo } from 'react-icons/fa'
+
+function SideBar() {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<FaPoo size="28" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+      <SideBarIcon icon={<BsGearFill size="28" />} />
+      <SideBarIcon icon={<BsPlus size="28" />} />
+    </div>
+  )
+}
+
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+)
+
+export default SideBar
